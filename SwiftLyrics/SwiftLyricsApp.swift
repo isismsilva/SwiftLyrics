931261096b52e915153d5f6a51cc3f1a355a6c17
Isis Silva <isis.silva@powerhrg.com>
2023-02-05 13:52:21 -0300

    Initial Commit

---
+++ SwiftLyrics/SwiftLyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLyricsApp.swift
+//  SwiftLyrics
+//
+//  Created by Isis Silva on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
